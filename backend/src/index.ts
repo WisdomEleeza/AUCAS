70b899e23c0a8cd05a07811b766c78dd56a79b16
Wisdom Eleeza <whiy07@gmail.com>
2023-12-13 23:58:32 +0000

    Work on connecting port to the server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,11 @@
-import 'dotenv/config'
-import 'module-alias/register'
+import 'dotenv/config';
+import 'module-alias/register';
+import express from 'express';
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.listen(port, () => console.log(`Server Started on port ${port}`))
