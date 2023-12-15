772a33305b61f737d47d305f42466685ad618b2c
Wisdom Eleeza <whiy07@gmail.com>
2023-12-15 17:12:19 +0000

    Fix errors, intall packages and ignore files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,16 @@
 import 'dotenv/config';
 import 'module-alias/register';
 import express from 'express';
-import errorHandler from '@/middleware/error.middleware'
+import errorHandler from '@/middleware/error.middleware';
+import helmet from 'helmet';
 const port = process.env.PORT || 8080;
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
 
+app.use(errorHandler);
 
-
-app.use(errorHandler)
-
-
-
-
-
-
-
-
-app.listen(port, () => console.log(`Server Started on port ${port}`))
+app.listen(port, () => console.log(`Server Started on port ${port}`));
